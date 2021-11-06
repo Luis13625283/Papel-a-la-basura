b107db97c46c0f56bad49860331de8f9fada7373
Luis13625283 <90009089+Luis13625283@users.noreply.github.com>
2021-11-05 18:21:59 -0600

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 const Render = Matter.Render;
-var dustbinObj,groundObject	
+var dustbinObj, paperObject,groundObject;
 var world;
 var paper;
 
@@ -16,14 +16,21 @@ function setup() {
 
 	engine = Engine.create();
 	world = engine.world;
-	
-	groundObject=new ground(width/2,670,width,20);
-	dustbinObj=new dustbin(1200,650);
 
-	Engine.run(engine);
+	paperObject=new 
+	paper(200,450,70); 
+	groundObject=new 
+	ground(width/2,670,width,20); 
+	dustbinObj=new dustbin(1200,650);
 
 	paper = new Paper(200,200);
   
+
+	paperObject=new paper(200,450,70);
+
+	Render = Matter.Render;
+	Engine.run(engine);
+	Render.run(render);
 }
 
 
@@ -31,11 +38,12 @@ function draw() {
   rectMode(CENTER);
   background(230);
  
-
+  paperObject.display();
+  
   groundObject.display();
   dustbinObj.display();
 
-  paper.display();
+  paperObject.display();
 
 }
 
